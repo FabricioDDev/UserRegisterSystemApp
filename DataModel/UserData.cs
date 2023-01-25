ba54f3d0ccd6a13c4bff4415b0b0341d787457bf
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2023-01-25 14:57:19 +0100

    UserData-Properties-Created

---
+++ DataModel/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel;
+using DB;
+
+namespace DataModel
+{
+    public class UserData
+    {
+        public UserData() { data = new DataAccess(); }
+        private DataAccess data;
+    }
+}
