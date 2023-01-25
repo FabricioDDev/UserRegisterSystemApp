39569bbced0ea8cbee2199f138527b1273a03464
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2023-01-25 12:24:58 +0100

    DataAccess-and-Its-Properties-Created

---
+++ DB/DataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DB
+{
+    public class DataAccess
+    {
+        public DataAccess()
+        {
+            connection = new SqlConnection("server=BRENDA-PC; database=UserRegister; integrity security=true");
+            command = new SqlCommand();
+        }
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        public SqlDataReader readerProp
+        {
+            get { return reader; }
+        }
+    }
+}
