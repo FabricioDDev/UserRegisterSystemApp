d241f20dbb7320ecb1257b8c0c8de561249bc125
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2023-01-25 12:36:03 +0100

    DataAccess-Methods-Created

--- DB/DataAccess.cs
+++ DB/DataAccess.cs
@@ -21,5 +21,45 @@ namespace DB
         {
             get { return reader; }
         }
+
+        public void Query(string query)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = query;
+        }
+        public void SP(string sp)
+        {
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.CommandText = sp;
+        }
+        public void Read()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch(Exception ex) { throw ex; }
+        }
+        public void Execute()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex) { throw ex; }
+        }
+        public void Parameters(string parameter, object value)
+        {
+            command.Parameters.AddWithValue(parameter, value);
+        }
+        public void Close()
+        {
+            if (reader != null) reader.Close();
+            connection.Close();
+        }
     }
 }
