d9af1f979fe6b86850fa4b430fc443bf97ffef9c
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2023-01-25 14:51:02 +0100

    UserClass-Properties-Created

---
+++ DomainModel/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class User
+    {
+        private int Id;
+        private string Email;
+        private string UserName;
+        private string Password;
+
+        public int idProp
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        public string emailProp
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+        public string userNameProp
+        {
+            get { return UserName; }
+            set { UserName = value; }
+        }
+        public string passwordProp
+        {
+            get { return Password; }
+            set { Password = value; }
+        }
+        public string ImageProfile { get; set; }
+        public Role RoleType { get; set; }
+    }
+}
