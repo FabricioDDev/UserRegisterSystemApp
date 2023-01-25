70359ca6ae63e3325e04d1d8deba9c15104134d3
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2023-01-25 12:48:55 +0100

    RoleClass-Properties-Created

---
+++ DomainModel/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public override string ToString()
+        {
+            return RoleName;
+        }
+    }
+}
