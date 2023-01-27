469e6d30a0dc8520a4ca89f782c27c174ab07e4e
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2023-01-27 15:24:58 +0100

    FrmErrorPage-cs-completed

---
+++ UserRegisterSystem/FrmErrorPage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmErrorPage.aspx.cs" Inherits="UserRegisterSystem.FrmErrorPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Oh No! some is Wrong...</h1>
+            <asp:LinkButton ID="LkbLogIn" OnClick="LkbLogIn_Click" runat="server">Go to the LogIn Page</asp:LinkButton>
+        </div>
+    </form>
+</body>
+</html>
---
+++ UserRegisterSystem/FrmErrorPage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DomainModel;
+
+namespace UserRegisterSystem
+{
+    public partial class FrmErrorPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+            if (!Security.isErrorSessionActive(Session["Error"])){
+                Response.Redirect("FrmLogIn.aspx", false);
+            }
+        }
+
+        protected void LkbLogIn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("FrmLogIn.aspx", false);
+        }
+    }
+}
